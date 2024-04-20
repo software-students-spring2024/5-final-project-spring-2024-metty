73bf5db0cf510d0a6a9ac960b91509a432b37e88
iltenahmet <iltenahmet@gmail.com>
2024-04-20 12:51:28 -0400

    add authentication and db files, update pipfile and requierements

--- web_app/app.py
+++ web_app/app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask, render_template
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from db import *
+from auth import *
+
+load_dotenv()
 
 app = Flask(__name__)
 
-# Connect to MongoDB
-client = MongoClient(os.getenv("MONGO_URI", "mongodb://mongodb:27017/"))
-db = client.test
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -13,5 +17,6 @@ def index():
     """
     return render_template("home.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port="5000", host="0.0.0.0")
---
+++ web_app/db.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
+
+load_dotenv()
+
+"""
+TODO: Uncomment this part once we have our URI and DBNAME set up
+
+uri = os.getenv("MONGO_URI")
+db_name = os.getenv("MONGO_DBNAME")
+
+# Display error message if mongoDB environment is not set up
+if not uri or not db_name:
+    error_message = "MongoDB environment is not set up. Please check your environment variables."
+    app.logger.error(error_message)
+    raise EnvironmentError(error_message)
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+db = client[db_name]
+"""
+
+# Connect to MongoDB
+client = MongoClient(os.getenv("MONGO_URI", "mongodb://mongodb:27017/"))
+db = client.test
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Connected to MongoDB!")
+except Exception as e:
+    print("MongoDB connection error:", e)
