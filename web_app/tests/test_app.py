9b7eefbec755157ed20a4ccd8ac82c0e732f973a
al6862 <al6862@nyu.edu>
2024-04-22 13:53:20 -0400

    added initial tests

---
+++ web_app/tests/test_app.py
@@ -0,0 +1,8 @@
+class Tests:
+    """Class for testing app.py"""
+
+    def test_sanity_check(self):
+        """Function sanity check."""
+        expected = True
+        actual = True
+        assert actual == expected, "Expected True to be equal to True!"
