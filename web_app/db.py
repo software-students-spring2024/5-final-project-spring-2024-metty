983c533b5e6a5079b34fd6b061ac206f4ef3a2c9
al6862 <al6862@nyu.edu>
2024-04-24 15:52:51 -0400

    uncommented db

--- web_app/db.py
+++ web_app/db.py
@@ -1,16 +1,16 @@
-# from dotenv import load_dotenv
-# import os
-# from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
 
-# load_dotenv()
+load_dotenv()
 
-# # Connect to MongoDB
-# client = MongoClient(os.getenv("MONGO_URI", "mongodb://mongodb:27017/"))
-# db = client["final_project"]
+# Connect to MongoDB
+client = MongoClient(os.getenv("MONGO_URI", "mongodb://mongodb:27017/"))
+db = client["final_project"]
 
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command("ping")
-#     print("Connected to MongoDB!")
-# except Exception as e:
-#     print("MongoDB connection error:", e)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Connected to MongoDB!")
+except Exception as e:
+    print("MongoDB connection error:", e)
