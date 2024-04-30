7d748b554b977622943e6af0955afa85e669b84d
iltenahmet <iltenahmet@gmail.com>
2024-04-30 14:18:01 -0400

    add tests for login, signup and index routes

--- web_app/tests/test_app.py
+++ web_app/tests/test_app.py
@@ -1,3 +1,5 @@
+from web_app.app import app
+
 class Tests:
     """Class for testing app.py"""
 
@@ -6,3 +8,18 @@ class Tests:
         expected = True
         actual = True
         assert actual == expected, "Expected True to be equal to True!"
+
+    def test_index(self):
+         """Function testing index."""
+         response = app.test_client().get("/")
+         assert response.status_code == 200
+
+    def test_render_signup(self):
+         """Function testing rendering signup page."""
+         response = app.test_client().get("/signup")
+         assert response.status_code == 200
+
+    def test_render_login(self):
+         """Function testing rendering login page."""
+         response = app.test_client().get("/login")
+         assert response.status_code == 200
