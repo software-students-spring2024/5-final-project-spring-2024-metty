08678261e73ee623fd0b7cbaeecffd280d32f9d8
shriyakalakata <shriyakalakata123@gmail.com>
2024-04-30 21:34:34 -0400

    Fix the order of days in graph

--- web_app/static/script.js
+++ web_app/static/script.js
@@ -8,10 +8,16 @@ $(document).ready(function(){
                 $('#hoursWorked').text(response.hours_week);
                 $('#daysWorked').text(response.days);
                 $('#popup').show();
+
+                // Sort the keys of the data object to match the order of days
+                var sortedData = {};
+                ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].forEach(function(day) {
+                    sortedData[day] = response.day_hours[day] || 0;
+                });
     
                 // Render charts asynchronously after showing the popup
                 setTimeout(function() {
-                    drawChart('dayHoursChart', response.day_hours);
+                    drawChart('dayHoursChart', sortedData);
                 }, 0);
             }
         });
@@ -30,8 +36,8 @@ $(document).ready(function(){
 });
 
 function drawChart(canvasId, data) {
-    var labels = Object.keys(data);
     var values = Object.values(data);
+    var labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
     var ctx = document.getElementById(canvasId).getContext('2d');
     var chart = new Chart(ctx, {
@@ -39,7 +45,7 @@ function drawChart(canvasId, data) {
         data: {
             labels: labels,
             datasets: [{
-                label: 'Data',
+                label: 'Time (hours)',
                 data: values,
                 backgroundColor: 'rgba(191, 176, 173, 1)',
                 pointBackgroundColor: 'rgba(250, 250, 250, 1)',
@@ -47,20 +53,22 @@ function drawChart(canvasId, data) {
         },
         options: {
             scales: {
+                // axes styling
                 x: {
                     grid: {
                         beginAtZero: true,
-                        color: 'rgba(70, 51, 49, 0.2)', // X-axis gridline color
-                        borderColor: 'rgba(70, 51, 49, 1)', // X-axis border color
-                        borderWidth: 20 // X-axis border width
-                    }
+                        color: 'rgba(70, 51, 49, 0.2)',
+                        borderColor: 'rgba(70, 51, 49, 1)',
+                        borderWidth: 20
+                    },
+                    indexAxis: 'x'
                 },
                 y: {
                     grid: {
                         beginAtZero: true,
-                        color: 'rgba(70, 51, 49, 0.2)', // Y-axis gridline color
-                        borderColor: 'rgba(70, 51, 49, 1)', // Y-axis border color
-                        borderWidth: 20 // Y-axis border width
+                        color: 'rgba(70, 51, 49, 0.2)',
+                        borderColor: 'rgba(70, 51, 49, 1)',
+                        borderWidth: 20
                     }
                 }
             }
